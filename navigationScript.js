a529d6de45e9cab4d39a50b263fd93da803b1163
msmiah019 <131150107+msmiah019@users.noreply.github.com>
2023-04-18 10:45:43 +0100

    Delete navigationScript.js

--- navigationScript.js
+++
@@ -1,8 +0,0 @@
-var smallDeviceMenu = document.getElementById("smallDeviceMenu")
-function showMenu() {
-    smallDeviceMenu.style.right = "0px";
-
-}
-function hideMenu() {
-    smallDeviceMenu.style.right = "-250px";
-}
